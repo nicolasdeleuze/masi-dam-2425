c8db0f84659ec2bf9e95f7558f2197e46a3de3ec
nicolasdeleuze <nicolas.deleuze@student.hepl.be>
2024-11-17 17:10:34 +0100

    dev lib comm

--- lib/comm/com_service.dart
+++ lib/comm/com_service.dart
@@ -1,6 +1,4 @@
-import 'dart:io';
 import 'dart:async';
-import 'dart:isolate';
 
 import 'package:flutter_p2p_connection/flutter_p2p_connection.dart';
 
@@ -10,6 +8,10 @@ class ComService {
   String? _networkName;
   UserRole? _role;
   FlutterP2pConnection? _connection;
+  List<DiscoveredPeers> _peers = [];
+  WifiP2PInfo? _wifiP2PInfo;
+  StreamSubscription<WifiP2PInfo>? _streamWifiInfo;
+  StreamSubscription<List<DiscoveredPeers>>? _streamPeers;
 
   ComService() {
     _connection = FlutterP2pConnection();
@@ -64,4 +66,77 @@ class ComService {
 
   void startAsWaiter(String networkName) {
   }
+
+  void createGroup() async {
+    bool ret = await _connection!.createGroup();
+    if(ret == false)
+      throw Exception('Failed to create group');
+  }
+
+  void removeGroup() async {
+    bool ret = await _connection!.removeGroup();
+    if(ret == false)
+      throw Exception('Failed to leave group');
+  }
+
+  void discoverPeers() async {
+    bool ret = await _connection!.discover();
+    if(ret == false)
+      throw Exception('Failed to enable peers discovery');
+  }
+
+  void stopDiscoverPeers() async {
+    bool ret = await _connection!.stopDiscovery();
+    if(ret == false)
+      throw Exception('Failed to stop peers discovery');
+  }
+
+  Future<void> startSocket (
+      void Function(String, String) onConnect,
+      void Function(TransferUpdate) transferUpdate,
+      void Function(dynamic) receiveString,
+      ) async {
+    if(_wifiP2PInfo != null) {
+      bool started = await _connection!.startSocket(
+        groupOwnerAddress: _wifiP2PInfo!.groupOwnerAddress,
+        downloadPath: '/storage/emulated/0/Download',
+        maxConcurrentDownloads: 2,
+        deleteOnError: true,
+        onConnect: onConnect,
+        transferUpdate: transferUpdate,
+        receiveString: receiveString,
+      );
+      if(!started)
+        throw Exception('Failed to start socket');
+    }
+  }
+
+  Future<void> connectToSoecket(
+      void Function(String) onConnect,
+      void Function(TransferUpdate) transferUpdate,
+      void Function(dynamic) receiveString,
+      ) async {
+    await _connection!.connectToSocket(
+      groupOwnerAddress: _wifiP2PInfo!.groupOwnerAddress,
+      downloadPath: '/storage/emulated/0/Download',
+      maxConcurrentDownloads: 2,
+      deleteOnError: true,
+      onConnect: onConnect,
+      transferUpdate: transferUpdate,
+      receiveString: receiveString,
+    );
+  }
+
+  Future<void> closeSocket() async {
+    bool s = await _connection!.closeSocket();
+    if(!s)
+      throw Exception('Failed to close socket');
+  }
+
+  Future<void> sendString(String message) async {
+    bool s = await _connection!.sendStringToSocket(message);
+    if(!s)
+      throw Exception('Failed to send message');
+  }
+
 }
