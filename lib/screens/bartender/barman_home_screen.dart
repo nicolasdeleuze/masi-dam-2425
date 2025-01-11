b1915bf4a65c95912f3cb834d5855eaa921ca8be
nicolasdeleuze <nicolas.deleuze@student.hepl.be>
2025-01-11 17:10:25 +0100

    dev fusion barman & orders à tester + ajout safearea

--- lib/screens/bartender/barman_home_screen.dart
+++ lib/screens/bartender/barman_home_screen.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:masi_dam_2425/comm/com_service.dart';
+import 'package:masi_dam_2425/model/order.dart';
 import 'package:masi_dam_2425/model/roles.dart';
 import 'package:masi_dam_2425/widgets/containers/header_container_widget.dart';
 import 'package:masi_dam_2425/widgets/loader_widget.dart';
+import 'package:masi_dam_2425/widgets/order_list_widget.dart';
 
 /// A widget representing the Bartender's home screen.
 /// Displays a list of orders and provides options to see one in particular.
@@ -19,6 +21,10 @@ class BarmanHomeWidget extends StatefulWidget {
     return cs;
   }
 
+  // TODO
+  // à lier avec backend
+  List<Order> orders = [];
+
   @override
   State<BarmanHomeWidget> createState() => _BarmanHomeWidgetState();
 }
@@ -29,25 +35,29 @@ class _BarmanHomeWidgetState extends State<BarmanHomeWidget> {
     double width = MediaQuery.of(context).size.width;
     widget.comService.setContext(context);
     return Scaffold(
-      body: FutureBuilder(
-        future: widget.initialize_p2p_root(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.done) {
-            return Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                HeaderContainer(
-                    width: width,
-                    subtitle: 'Bartender',
-                    userID : "RLE1234"
-                ),
-                Spacer()
-              ],
-            );
-          } else {
-            return LoaderWidget();
-          }
-        },
+      body: SafeArea(
+        child: FutureBuilder(
+          future: widget.initialize_p2p_root(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.done) {
+              return Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  HeaderContainer(
+                      width: width,
+                      subtitle: 'Bartender',
+                      userID : "RLE1234"
+                  ),
+                  Expanded(
+                      child: OrderListView(orders: widget.orders)
+                  )
+                ],
+              );
+            } else {
+              return LoaderWidget();
+            }
+          },
+        ),
       ),
     );
   }
